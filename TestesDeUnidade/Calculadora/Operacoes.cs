19627932f4335e7164a70d7a05e33b5de8b08f49
Luiz Strobelt <lhsperez@gmail.com>
2019-02-20 00:47:52 -0300

    Exemplo de TestesDeUnidade

---
+++ TestesDeUnidade/Calculadora/Operacoes.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculadora
+{
+    public class Operacoes
+    {
+        public int Somar(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+    }
+}
---
+++ TestesDeUnidade/TestesDeUnidades/OperacoesTestes.cs
@@ -0,0 +1,18 @@
+﻿using Calculadora;
+using NUnit.Framework;
+
+namespace TestesDeUnidade
+{
+    [TestFixture]
+    public class OperacoesTestes
+    {
+        [Test]
+        public void SomarDeveRetornarASomaCorretaDeDoisNumeros()
+        {
+            Operacoes operacoes = new Operacoes();
+            var resultado = operacoes.Somar(1, 2);
+
+            Assert.AreEqual(resultado, 3);
+        }
+    }
+}
